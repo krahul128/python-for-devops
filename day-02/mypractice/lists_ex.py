8cd4c43512af708eff35cb4f6a2544d7a8134452
128rahulk <128rahulk@gmail.com>
2026-03-28 22:13:13 +0530

    learning list

---
+++ day-02/mypractice/lists_ex.py
@@ -0,0 +1,26 @@
+a=[100,200,3.1,True]
+
+print(type(a))
+print(a)
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+
+
+a = [100, 200, 3.1, True]
+a.append(500)                                                               
+for item in a:
+    print(item)
+
+
+
+clouds=list()
+clouds.append("aws")
+clouds.append("azure")
+
+#for item in clouds:
+print(clouds) 
+print("length of cloud list:", len(clouds))
+print(dir(clouds))
+print(clouds.count.__doc__)
